b7d0d7146debe5391825747970ae4321bc8f6bc6
Aidana Khankel <aidanakhankeldieva91@gmail.com>
2021-02-15 21:31:41 -0600

    add client and server app

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recipient: "",
+      textmessage: "",
+    };
+  }
+
+  sendText = (e) => {
+    const { recipient, textmessage } = this.state;
+
+    //pass var
+    fetch(
+      `http://localhost:5000/send-text?recipient=${recipient}&textmessage=${textmessage}`
+    ).catch((err) => console.log(err));
+    this.setState({ recipient: "" });
+    this.setState({ textmessage: "" });
+  };
+
+  render() {
+    const { recipient, textmessage } = this.state;
+
+    return (
+      <div className='App'>
+        <h1 className='textheader'>Send a message</h1>
+        <form action='' onSubmit={(e) => e.preventDefault()}>
+          <h3>Send to:</h3>
+          <input
+            type='text'
+            placeholder='enter your number'
+            value={recipient}
+            onChange={(e) => this.setState({ recipient: e.target.value })}
+          />
+
+          <h3>Message: </h3>
+          <textarea
+            name=''
+            id=''
+            cols='30'
+            rows='5'
+            placeholder='your message'
+            value={textmessage}
+            onChange={(e) => this.setState({ textmessage: e.target.value })}
+          ></textarea>
+
+          <button onClick={this.sendText}>Send text</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,47 @@
+//install dependencies: $npm install express cors twilio
+
+const express = require("express");
+//const path = require("path");
+const cors = require("cors");
+const twilio = require("twilio");
+const app = express();
+
+//twilio requirements  -- use dotenv
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH;
+const client = new twilio(accountSid, authToken);
+
+//utilize cors, blockes the browser from restricting any data
+app.use(cors());
+app.use(express.json());
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin");
+// });
+
+app.use(express.static("public"));
+
+//welcome page
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Server");
+});
+
+//twilio text
+app.get("/send-text", (req, res) => {
+  //_GET variables, passed via query string
+  const { recipient, textmessage } = req.query;
+
+  //send text
+  client.messages
+    .create({
+      body: textmessage,
+      to: `+1${recipient}`,
+      from: "+14023833377", //twilio number /
+    })
+    .then((message) => console.log(message.body));
+});
+
+//set up port to listen
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server is running on port ", port));
